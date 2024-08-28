f8d3dde91d931f13f95893492cc68651526db99e
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-08-28 10:49:17 -0400

    Add envs

--- next.config.js
+++ next.config.js
@@ -5,26 +5,9 @@ const withNextIntl = require('next-intl/plugin')();
 const nextConfig = {
     reactStrictMode: false,
     env: {
-        //Mock Server local host and port number 
-        
-        REACT_APP_API_HOST: 'http://localhost',
-        REACT_APP_API_PORT:':8085',
-       
 
-        //Production Server host and port number
-
-        // REACT_APP_API_HOST: 'https://ec2-3-13-148-183.us-east-2.compute.amazonaws.com:',
-        // REACT_APP_API_PORT:'8080',
-       
-
-        // Local Server host and port number
-
-        // REACT_APP_API_HOST: 'http://localhost:',
-        // REACT_APP_API_PORT:'8080',
-       
-
-        },
+    },
 }
 
- 
+
 module.exports = withNextIntl(nextConfig);
