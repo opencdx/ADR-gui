c61fc61dcb5aa5cf25d389521fbf57a5a4be481d
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-10-01 14:24:20 -0400

    Update delete icons to use library

--- src/components/query-builder/join-operation-box.tsx
+++ src/components/query-builder/join-operation-box.tsx
@@ -1,6 +1,7 @@
 import { JoinOperation } from "@/api/adr";
 import { useQueryStore } from "@/lib/store";
 import { FC } from "react";
+import { DeleteIcon } from "ui-library";
 import { JoinOperationRender } from "../ui/join-operation-render";
 
 export interface JoinOperationBoxProps {
@@ -25,7 +26,7 @@ export const JoinOperationBox: FC<JoinOperationBoxProps> = ({ joinOperation, ind
         <div>
             <div className='flex rounded-md h-12 w-auto px-4 py-2 items-center justify-between border border-[#757575] mb-2 bg-white'>
                 <div className='flex'><span className="material-symbols-outlined" style={{ color: "#757575" }}>drag_indicator</span><p style={{ color: "#001124" }}><JoinOperationRender joinOperation={joinOperation}/></p></div>
-                <div><span onClick={() => handleRemove(index)} className="material-symbols-outlined" style={{ color: "#757575", cursor: "pointer" }}>delete</span></div>
+                <div className='flex cursor-pointer text-[#757575]' onClick={() => handleRemove(index)}><DeleteIcon /></div>
             </div>
         </div>
     )
