9a0bef2a963b9184543c826caf66a1a132a88b09
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-10-04 12:03:30 -0400

    Remove unneeded parent div

--- src/app/query-builder/page.tsx
+++ src/app/query-builder/page.tsx
@@ -1,10 +1,8 @@
 import QueryBuilder from '@/components/query-builder/query-builder';
-import Head from 'next/head';
+
 export default function QueryBuilderPage() {
   return (
-    <div>
-      <QueryBuilder />
-    </div>
+    <QueryBuilder />
   );
 
 }
