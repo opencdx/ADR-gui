2fa281e972be66dc002fa357162b7206ceb6689d
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2024-10-04 11:36:13 -0400

    Update icons to use ui-library

--- src/components/droppable/group-droppable.tsx
+++ src/components/droppable/group-droppable.tsx
@@ -1,10 +1,9 @@
 
 
-import { useState, type FC } from 'react'
-import { useDrag } from 'react-dnd'
 import { Query } from "@/api/adr";
-
-
+import { useState, type FC } from 'react';
+import { useDrag } from 'react-dnd';
+import { AddIcon } from 'ui-library';
 import { DroppableTypes } from './droppable-types';
 
 export interface GroupDroppableProps {
@@ -31,7 +30,7 @@ export const GroupDroppable: FC<GroupDroppableProps> = ({ showCopyIcon, group })
   return (
     <>
       <div ref={drag} className='cursor-pointer mr-3 rounded-md border-2 border-blue-500 inline-flex h-8 px-3 justify-center items-center gap-3 shrink-0 text-blue-500 font-medium' style={{ opacity }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-        <span className="material-symbols-outlined">add</span>
+        <AddIcon />
         Add Grouping
       </div>
     </>
